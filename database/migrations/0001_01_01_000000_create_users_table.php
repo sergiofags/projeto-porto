06e8b5a4506994950e68b4af5b95f600e3dc9bc3
Sergio Fagundes <contato.sergiofagundes@gmail.com>
2025-04-29 14:50:09 +0000

    Add tipo_perfil in RegisteredUserController

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            $table->enum('tipo_perfil', ['Candidato', 'Admin', 'SuperAdmin'])->default('Candidato');
+
             $table->rememberToken();
             $table->timestamps();
         });
