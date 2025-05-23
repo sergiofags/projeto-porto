5bec808c8008e260155401ffabd07c08ddf0f492
Caroline <149633656+carolineSilvaC@users.noreply.github.com>
2025-05-23 14:55:50 +0000

    feature/tela-processo

--- resources/js/components/app-header.tsx
+++ resources/js/components/app-header.tsx
@@ -14,26 +14,26 @@ import { Link, usePage } from '@inertiajs/react';
 import { BookOpen, Folder, LayoutGrid, Menu, Search } from 'lucide-react';
 import AppLogoIcon from './app-logo-icon';
 
-const mainNavItems: NavItem[] = [
-    {
-        title: 'Dashboard',
-        href: '/dashboard',
-        icon: LayoutGrid,
-    },
-];
+// const mainNavItems: NavItem[] = [
+//     {
+//         title: 'Dashboard',
+//         href: '/dashboard',
+//         icon: LayoutGrid,
+//     },
+// ];
 
-const rightNavItems: NavItem[] = [
-    {
-        title: 'Repository',
-        href: 'https://github.com/laravel/react-starter-kit',
-        icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits',
-        icon: BookOpen,
-    },
-];
+// const rightNavItems: NavItem[] = [
+//     {
+//         title: 'Repository',
+//         href: 'https://github.com/laravel/react-starter-kit',
+//         icon: Folder,
+//     },
+//     {
+//         title: 'Documentation',
+//         href: 'https://laravel.com/docs/starter-kits',
+//         icon: BookOpen,
+//     },
+// ];
 
 const activeItemStyles = 'text-neutral-900 dark:bg-neutral-800 dark:text-neutral-100';
 
@@ -47,33 +47,33 @@ export function AppHeader({ breadcrumbs = [] }: AppHeaderProps) {
     const getInitials = useInitials();
     return (
         <>
-            <div className="border-sidebar-border/80 border-b">
+            <div className="border-sidebar-border/80 border-b bg-blue-600">
                 <div className="mx-auto flex h-16 items-center px-4 md:max-w-7xl">
                     {/* Mobile Menu */}
                     <div className="lg:hidden">
                         <Sheet>
                             <SheetTrigger asChild>
                                 <Button variant="ghost" size="icon" className="mr-2 h-[34px] w-[34px]">
-                                    <Menu className="h-5 w-5" />
+                                    <Menu className="h-5 w-5 text-white"/>
                                 </Button>
                             </SheetTrigger>
                             <SheetContent side="left" className="bg-sidebar flex h-full w-64 flex-col items-stretch justify-between">
                                 <SheetTitle className="sr-only">Navigation Menu</SheetTitle>
                                 <SheetHeader className="flex justify-start text-left">
-                                    <AppLogoIcon className="h-6 w-6 fill-current text-black dark:text-white" />
+                                    {/* <AppLogoIcon className="h-6 w-6 fill-current text-black dark:text-white" /> */}
                                 </SheetHeader>
                                 <div className="flex h-full flex-1 flex-col space-y-4 p-4">
                                     <div className="flex h-full flex-col justify-between text-sm">
-                                        <div className="flex flex-col space-y-4">
+                                        {/* <div className="flex flex-col space-y-4">
                                             {mainNavItems.map((item) => (
                                                 <Link key={item.title} href={item.href} className="flex items-center space-x-2 font-medium">
                                                     {item.icon && <Icon iconNode={item.icon} className="h-5 w-5" />}
                                                     <span>{item.title}</span>
                                                 </Link>
                                             ))}
-                                        </div>
+                                        </div> */}
 
-                                        <div className="flex flex-col space-y-4">
+                                        {/* <div className="flex flex-col space-y-4">
                                             {rightNavItems.map((item) => (
                                                 <a
                                                     key={item.title}
@@ -86,7 +86,7 @@ export function AppHeader({ breadcrumbs = [] }: AppHeaderProps) {
                                                     <span>{item.title}</span>
                                                 </a>
                                             ))}
-                                        </div>
+                                        </div> */}
                                     </div>
                                 </div>
                             </SheetContent>
@@ -98,7 +98,7 @@ export function AppHeader({ breadcrumbs = [] }: AppHeaderProps) {
                     <div className="ml-6 hidden h-full items-center space-x-6 lg:flex">
                         <NavigationMenu className="flex h-full items-stretch">
                             <NavigationMenuList className="flex h-full items-stretch space-x-2">
-                                {mainNavItems.map((item, index) => (
+                                {/* {mainNavItems.map((item, index) => (
                                     <NavigationMenuItem key={index} className="relative flex h-full items-center">
                                         <Link
                                             href={item.href}
@@ -115,7 +115,7 @@ export function AppHeader({ breadcrumbs = [] }: AppHeaderProps) {
                                             <div className="absolute bottom-0 left-0 h-0.5 w-full translate-y-px bg-black dark:bg-white"></div>
                                         )}
                                     </NavigationMenuItem>
-                                ))}
+                                ))} */}
                             </NavigationMenuList>
                         </NavigationMenu>
                     </div>
@@ -123,10 +123,10 @@ export function AppHeader({ breadcrumbs = [] }: AppHeaderProps) {
                     <div className="ml-auto flex items-center space-x-2">
                         <div className="relative flex items-center space-x-1">
                             <Button variant="ghost" size="icon" className="group h-9 w-9 cursor-pointer">
-                                <Search className="!size-5 opacity-80 group-hover:opacity-100" />
+                                <Search className="!size-5 text-white"/>
                             </Button>
                             <div className="hidden lg:flex">
-                                {rightNavItems.map((item) => (
+                                {/* {rightNavItems.map((item) => (
                                     <TooltipProvider key={item.title} delayDuration={0}>
                                         <Tooltip>
                                             <TooltipTrigger>
@@ -145,7 +145,7 @@ export function AppHeader({ breadcrumbs = [] }: AppHeaderProps) {
                                             </TooltipContent>
                                         </Tooltip>
                                     </TooltipProvider>
-                                ))}
+                                ))} */}
                             </div>
                         </div>
                         <DropdownMenu>
---
+++ resources/js/pages/process/inicio-processo.tsx
@@ -0,0 +1,129 @@
+import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
+import AppLayout from '@/layouts/app-layout';
+import { SharedData, type BreadcrumbItem } from '@/types';
+import { Head, Link, usePage } from '@inertiajs/react';
+import TextLink from '@/components/text-link';
+
+import { BreadcrumbAuto } from '@/components/ui/breadcrumb-auto'
+import { useEffect, useState } from 'react';
+import { Button } from '@/components/ui/button';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Início',
+        href: '/inicio-processo',
+    },
+];
+
+type Processo = {
+    id: number;
+    nome: string;
+    descricao: string;
+};
+
+type Props = {
+    processos?: Processo[];
+    user?: {
+        name: string;
+    };
+};
+
+export default function Inicio({ processos = [], user }: Props) {
+    const pathname = typeof window !== 'undefined' ? window.location.pathname : '';
+    const segments = pathname.split('/').filter(Boolean);
+
+    const [process, setProcess] = useState([]);
+    const {auth}=usePage<SharedData>().props;
+    //const [data] = useState({name: auth.user.name});
+    const nomeCompleto = auth.user.name;
+    const partes = nomeCompleto.trim().split(' ');
+    const nome =partes[0];
+
+    useEffect(() => {
+        async function getProcess() {
+            const url = "https://localhost:8000/api/process";
+            try {
+                const response = await fetch(url);
+                if (!response.ok) {
+                    throw new Error(`Response status: ${response.status}`);
+                }
+
+                const json = await response.json();
+                setProcess(json);
+            } catch (e) {
+                console.log('Deu erro', e);
+            }
+        }
+
+        getProcess();
+    }, []);
+    
+
+    return (
+        <AppLayout>
+            <Head title="Início" />
+            
+            <div className="flex h-full max-h-full flex-1 flex-col gap-4 rounded-xl p-4">
+                
+                <nav className="text-sm text-muted-foreground mb-4">
+                    <ol className="flex items-center space-x-2">
+                        <li>
+                            <Link href="/" className="hover:underline">Início</Link>
+                        </li>
+                        {segments.map((segment, index) => {
+                            const href = '/' + segments.slice(0, index + 1).join('/');
+                            const isLast = index === segments.length - 1;
+                            const label = segment.charAt(0).toUpperCase() + segment.slice(1).replace(/-/g, ' ');
+
+                            return (
+                                <li key={href} className="flex items-center space-x-2">
+                                    <span className="mx-1">/</span>
+                                    {isLast ? (
+                                        <span className="font-medium">{label}</span>
+                                    ) : (
+                                        <Link href={href} className="hover:underline">{label}</Link>
+                                    )}
+                                </li>
+                            );
+                        })}
+                    </ol>
+
+                    <h1 className="text-2xl font-semibold mt-4 text-black">Olá, {nome}.</h1>
+
+                </nav>
+
+                <div className="border-sidebar-border/70 dark:border-sidebar-border relative max-h-[100vh] flex-1 overflow-hidden rounded-xl border md:min-h-min flex items-center justify-center ">
+
+                {processos.length === 0 ? (
+                    <div className="text-center flex items-center justify-center h-full px-4">
+                        <div className="tracking-wide max-w-md w-full break-words whitespace-normal">
+                            <h2 className="text-xl font-semibold block leading-tight text-black break-words">
+                            No momento não há processos cadastrados
+                            </h2>
+                            <hr className="mt-4 mb-4 w-full bg-blue-600 h-0.5" />
+                            <p className="text-sm text-blue-600 mt-1">
+                            Clique no botão para adicionar um processo
+                            </p>
+                            <Button className="p-4 sm:p-6 bg-blue-600 hover:bg-blue-800 mt-4 text-sm sm:text-base">
+                            Adicionar processo <span className="ml-1">＋</span>
+                            </Button>
+                        </div>
+                    </div>
+
+                ) : (
+                    <div className="grid gap-4">
+                        {processos.map((processo) => (
+                            <div key={processo.id} className="border rounded p-4">
+                                <h3 className="font-semibold">{process.nome}</h3> {/* substitua por campo correto */}
+                                <p>{process.descricao}</p>
+                                <p>{process.numero_processo}</p>
+                            </div>
+                        ))}
+                    </div>
+                )}
+                </div>
+
+            </div>
+        </AppLayout>
+    );
+}
