f9eadfb2fb1971b1377ce8d5ff4b716e781284fc
Caroline <149633656+carolineSilvaC@users.noreply.github.com>
2025-06-12 13:46:49 +0000

    detalhes da vaga

--- resources/js/pages/vacancy/acoes/detalhes-da-vaga.tsx
+++ resources/js/pages/vacancy/acoes/detalhes-da-vaga.tsx
@@ -4,6 +4,7 @@ import { SharedData } from '@/types';
 import { Head, Link, usePage } from '@inertiajs/react';
 import { useEffect, useState } from 'react';
 import { Button } from '@/components/ui/button';
+import { ChevronLeft } from 'lucide-react';
 
 export default function DetalhesVaga() {
 
@@ -54,34 +55,80 @@ export default function DetalhesVaga() {
     return (
         <AppLayout>
             <Head title="Detalhes da Vaga" />
-            <div className="flex flex-row gap-2">
-                <Link href={`/processo/vagas?id=${processId}`} className="w-full">
-                    <Button type="button" variant="secondary">
-                        Voltar
-                    </Button>
-                </Link>
-            </div>
 
-            <div className="grid gap-4">
-                <div className="border rounded p-4">
+            <div className="grid gap-4 p-4">
+                <div className="border rounded p-4 pl-10 pr-10">
                 
                     {vaga.map((item) => (
                         <div key={item.id}>
-                            <h2 className="text-xl font-bold">{item.titulo}</h2>
-                            <p><strong>Responsabilidades:</strong> {item.responsabilidades || 'Não informado'}</p>
-                            <p><strong>Requisitos:</strong> {item.requisitos || 'Não informado'}</p>
-                            <p><strong>Carga Horária:</strong> {item.carga_horaria}</p>
-                            <p><strong>Remuneração:</strong> R$ {item.remuneracao.toFixed(2)}</p>
-                            <p><strong>Benefícios:</strong> {item.beneficios || 'Não informado'}</p>
-                            <p><strong>Quantidade:</strong> {item.quantidade}</p>
-                            <p><strong>Data de Início:</strong> {item.data_inicio}</p>
-                            <p><strong>Data de Fim:</strong> {item.data_fim}</p>
-                            <p><strong>Status:</strong> {item.status}</p>
-                            <p><strong>Tipo de Vaga:</strong> {item.tipo_vaga}</p>
+                            <h1 className="text-3xl  mt-6">{item.titulo}</h1>
+
+                            <div className="flex flex-col sm:flex-row gap-2 mt-6 mb-6">
+                                <Button type="button" className="flex-1 p-4 sm:p-6 bg-[#008DD0] hover:bg-[#0072d0] mt-4 text-sm">
+                                    <Link href={``} className="w-full">
+                                    Ver candidatos
+                                    </Link>
+                                </Button>
+                                <Button type="button" className="flex-1 p-4 sm:p-6  bg-[#008DD0] hover:bg-[#0072d0] mt-4 text-sm ">
+                                    <Link href={``} className="w-full">
+                                    Classificação
+                                    </Link>
+                                </Button>
+                                <Button type="button" className="flex-1 p-4 sm:p-6 bg-green-500 hover:bg-green-600 mt-4 text-sm ">
+                                    <Link href={``} className="w-full">
+                                    Editar
+                                    </Link>
+                                </Button>
+                                <Button type="button" className="flex-1 p-4 sm:p-6  bg-red-600 hover:bg-red-800 mt-4 text-sm ">
+                                    <Link href={``} className="w-full">
+                                    Fechar
+                                    </Link>
+                                </Button>
+                            </div>
+
+                            <h1 className="text-xl"><strong>Responsabilidades:</strong></h1>
+                            <p>{item.responsabilidades || 'Não informado'}</p>
+                            
+                            <br></br>
+                            <h1 className="text-xl"><strong>Requisitos:</strong></h1>
+                            <p>{item.requisitos || 'Não informado'}</p>
+
+                            {/* <p><strong>Carga Horária:</strong> <br></br>{item.carga_horaria}</p>
+                            <p><strong>Remuneração:</strong> <br></br>R$ {item.remuneracao.toFixed(2)}</p> */}
+                            <br></br>
+                            <h1 className="text-xl"><strong>Benefícios:</strong></h1>
+                            <p>{item.beneficios || 'Não informado'}</p>
+
+                            <br></br>
+                            {/* <p><strong>Quantidade:</strong> <br></br>{item.quantidade}</p> */}
+                            <h1 className="text-xl"><strong>Data de Início:</strong></h1>
+                            <p>{item.data_inicio}</p>
+
+                            <br></br>
+                            <h1 className="text-xl"><strong>Data de Fim:</strong></h1>
+                            <p>{item.data_fim}</p>
+
+                            <br></br>
+                            <h1 className="text-xl"><strong>Status:</strong></h1>
+                            <p>{item.status}</p>
+
+                            <br></br>
+                            <h1 className="text-xl"><strong>Tipo de Vaga:</strong></h1>
+                            <p>{item.tipo_vaga}</p>
                         </div>
                     ))}
                     
+
+                    <div className="mt-6 mb-6">
+                        <Link className="w-fit flex" href={`/processo/vagas?id=${processId}`}>
+                            <Button
+                                className="flex items-center gap-2 rounded-md px-4 py-2 text-sm duration-200 bg-gray-500 text-white shadow-xs hover:bg-gray-600">
+                                <ChevronLeft /> Voltar
+                            </Button>
+                        </Link>
+                    </div>
                 </div>
+
             </div>
         </AppLayout> 
     );
